2493884b5b297d7d28bf944186c0bfd0cb7ff479
Trần An Thiên <anthien125@gmail.com>
2025-09-27 21:30:36 +0700

    Delete tuan5_2/tuan5 directory

--- tuan5_2/tuan5/src/main/java/vn/LTWeb/tuan5/config/StorageProperties.java
+++
@@ -1,9 +0,0 @@
-package vn.LTWeb.tuan5.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import lombok.Data;
-@Data
-@ConfigurationProperties("storage")
-public class StorageProperties {
-    private String location;
-}
--- tuan5_2/tuan5/src/main/java/vn/LTWeb/tuan5/exception/StorageFileNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package vn.LTWeb.tuan5.exception;
-
-public class StorageFileNotFoundException extends StorageException {
-    private static final long serialVersionUID = 1L;
-    public StorageFileNotFoundException(String message) {
-        super(message);
-    }
-}
-
